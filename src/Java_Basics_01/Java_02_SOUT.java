51e9baabe8c174f1d535609a7fa1c4d6a50c754c
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:11:38 +0200

    s.o.u.t

---
+++ src/Java_Basics_01/Java_02_SOUT.java
@@ -0,0 +1,18 @@
+package Java_Basics_01;
+
+public class Java_02_SOUT {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world !");
+		System.out.println("Hello world !");
+		System.out.println("Hello world !");
+		
+		System.out.print("Hello world !");
+		System.out.print("Hello world !");
+		System.out.print("Hello world !");
+		System.out.print("Hello world !");
+		
+
+	}
+
+} 
