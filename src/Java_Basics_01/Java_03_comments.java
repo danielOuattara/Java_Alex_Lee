15bcd42c5d12f4a2208271f1d4aef9846fee3250
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:11:47 +0200

    comments

---
+++ src/Java_Basics_01/Java_03_comments.java
@@ -0,0 +1,19 @@
+package Java_Basics_01;
+
+public class Java_03_comments {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+        /*
+        * this
+        * is
+        * a
+        * comment.
+        */
+
+        // this also a comment
+
+
+    }
+
+}
