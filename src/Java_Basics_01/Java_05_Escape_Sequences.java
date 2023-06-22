ac9e7b5522ebec7d5e1ca5690078eb4a10c1999e
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:12:49 +0200

    Escape character. Escape sequence

---
+++ src/Java_Basics_01/Java_05_Escape_Sequences.java
@@ -0,0 +1,12 @@
+package Java_Basics_01;
+
+public class Java_05_Escape_Sequences {
+
+	public static void main(String[] args) {
+		System.out.println("Dog");
+		System.out.print("Cat. ");
+		System.out.println("The \"fish was \"blue ");
+		System.out.println("These are double quotes \" \" ");
+		System.out.println("This is a back slash \\ ");
+	}
+} 
