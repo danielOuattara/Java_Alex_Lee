b69a52a2a4295cea0b23f77bfd81040f8e091f77
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:13:45 +0200

    long, float, double

---
+++ src/Java_Basics_01/Java_08_Long_Float_Double.java
@@ -0,0 +1,12 @@
+package Java_Basics_01;
+
+public class Java_08_Long_Float_Double {
+    public static void main(String[] args) {
+        // integer
+        int  quantity = 23;
+        long ssn = 234_567_357_373L;
+        float floating_value = 2.54F;
+        double pi = 3.1458141556367858;
+
+    }
+}
