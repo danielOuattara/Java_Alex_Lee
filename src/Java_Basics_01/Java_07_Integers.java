63f1e5ad4c2008a1fbd1d24524141387905264b1
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:13:24 +0200

    integer

---
+++ src/Java_Basics_01/Java_07_Integers.java
@@ -0,0 +1,13 @@
+package Java_Basics_01;
+
+public class Java_07_Integers {
+    public static void main(String[] args) {
+        System.out.println("Java_07_Int_Variables.main");
+        String name = "Alex Lee";
+        int age = 27;
+        System.out.println(name + " " +  age + " y.o");
+
+       // int pi = 3.14; // Incorrect
+        int negative = -365;
+    }
+}
