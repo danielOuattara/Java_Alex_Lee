e22c4ae1ee3e6618894b2492f05e1f7ca04be43d
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:12:11 +0200

    String

---
+++ src/Java_Basics_01/Java_04_String.java
@@ -0,0 +1,16 @@
+package Java_Basics_01;
+
+public class Java_04_String {
+
+	public static void main(String[] args) {
+		System.out.println("MadLibs!");
+		System.out.println("My sister __ sister  accidentally __ed all over her new ___");
+		
+		String adjective = "flamboyant";
+		String verb = "fart";
+		String noun = "pogo stick";
+		
+		System.out.println("My sister __ sister  accidentally __ed all over her new ___");
+
+	}
+} 
