47cd06c1a0e52abf03226b6769d2589357f42bf3
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 17:47:43 +0200

    11 Comparison Operators && logical operators

---
+++ src/Java_Basics_01/Java_11_Comparison_Operator.java
@@ -0,0 +1,33 @@
+package Java_Basics_01;
+
+public class Java_11_Comparison_Operator {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4;
+        if (a == b) {
+            System.out.println("number are equals");
+        } else if (a < b) {
+            System.out.println(a + " < " + b);
+        } else {
+            System.out.println(a + " > " + b);
+        }
+
+        // OR
+
+        boolean c = (a == b);
+        System.out.println("c : " + c);
+
+        /*
+        >  ,
+        >= ,
+        == ,
+        != ,
+        <  ,
+        <= ,
+        &&
+        ||
+         */
+    }
+
+}
