28558e7ae638646184959044190a1cefe6d3f2a1
danielOuattara <daniel.ouattara@gmx.com>
2023-06-23 10:24:19 +0200

    12 Exponents

---
+++ src/Java_Basics_01/Java_12_Exponents.java
@@ -0,0 +1,13 @@
+package Java_Basics_01;
+
+public class Java_12_Exponents {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(5, 2));
+        System.out.println(Math.pow(5, 0));
+        System.out.println(Math.pow(8, 5));
+        System.out.println(Math.pow(8, 5.5));
+        double exponent = Math.pow(2, 3);
+        System.out.println(exponent);
+
+    }
+}
