831375edc27493ec3a30e73016153e3131a9533b
danielOuattara <daniel.ouattara@gmx.com>
2023-06-23 10:25:35 +0200

    15 Arrays

---
+++ src/Java_Basics_01/Java_15_Arrays.java
@@ -0,0 +1,10 @@
+package Java_Basics_01;
+
+public class Java_15_Arrays {
+    public static void main(String[] args) {
+        // Zombie Killer
+        System.out.println("Welcome to our Zombie cleaner program");
+        String[] guns = {"Shotgun ", "Assault Rifle", "Sniper"};
+        String[] zombies = {"Close-Range ", "Mid-Range", "Long-Range"};
+    }
+}
