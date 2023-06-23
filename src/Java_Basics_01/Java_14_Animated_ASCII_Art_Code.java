198a6a3a5c0c40f61f6f87a0e623c92227662a72
danielOuattara <daniel.ouattara@gmx.com>
2023-06-23 10:25:16 +0200

    14 animated ASCII

---
+++ src/Java_Basics_01/Java_14_Animated_ASCII_Art_Code.java
@@ -0,0 +1,24 @@
+package Java_Basics_01;
+
+public class Java_14_Animated_ASCII_Art_Code {
+    public static void main(String[] args) throws InterruptedException {
+        // ROFLCOPTER
+
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+        Thread.sleep(1000);
+        System.out.println("ROFL:ROFL:ROFL:ROFL:ROFL ");
+
+    }
+}
