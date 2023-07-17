51e6be77b4f9b1671cc73699ad37bb4b9e77d1f1
danielOuattara <daniel.ouattara@gmx.com>
2023-07-17 21:12:08 +0200

    23: conditional operator

---
+++ src/Java_Basics_01/Java_23_Conditional_Operators.java
@@ -0,0 +1,14 @@
+package Java_Basics_01;
+
+public class Java_23_Conditional_Operators {
+    public static void main(String[] args) {
+        int a = 7 > 3 ? 7 : 3;
+        System.out.println(a);
+
+        String hello = "hello";
+
+        double result = hello.equals("Hello") ? 1 : 0;
+        System.out.println(result);
+
+    }
+}
