fa9171f74d2319960aa5167036da81485cda808e
danielOuattara <daniel.ouattara@gmx.com>
2023-07-17 21:11:44 +0200

    22: naming convention

---
+++ src/Java_Basics_01/Java_22_Naming_Convention.java
@@ -0,0 +1,18 @@
+package Java_Basics_01;
+
+public class Java_22_Naming_Convention {
+    static final int DAYS_IN_A_YEAR = 365;
+
+    public static void main(String[] args) {
+
+        /*
+        Names can contain letters, digits, underscores, and dollar signs
+        Names must begin with a letter
+        Names should start with a lowercase letter, and it cannot contain whitespace
+        Names can also begin with $ and _ (but we will not use it in this tutorial)
+        Names are case-sensitive ("myVar" and "myvar" are different variables)
+        Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+        */
+
+    }
+}
