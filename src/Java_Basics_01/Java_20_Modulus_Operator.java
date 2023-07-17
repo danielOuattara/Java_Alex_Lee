9752147255e543a1a37d1e624679b83e0322ccfd
danielOuattara <daniel.ouattara@gmx.com>
2023-07-17 19:29:02 +0200

    20: modulus operator

---
+++ src/Java_Basics_01/Java_20_Modulus_Operator.java
@@ -0,0 +1,10 @@
+package Java_Basics_01;
+
+public class Java_20_Modulus_Operator {
+    public static void main(String[] args) {
+        System.out.println(7 % 2);
+        System.out.println(17 % 3);
+        System.out.println(27 % 4);
+
+    }
+}
