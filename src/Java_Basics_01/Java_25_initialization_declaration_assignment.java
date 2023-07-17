1339c02b8f7aca2c738756e078251d17ecaf172e
danielOuattara <daniel.ouattara@gmx.com>
2023-07-17 21:13:12 +0200

    25: initialization declaration assignment

---
+++ src/Java_Basics_01/Java_25_initialization_declaration_assignment.java
@@ -0,0 +1,10 @@
+package Java_Basics_01;
+
+public class Java_25_initialization_declaration_assignment {
+    public static void main(String[] args) {
+        String pizza = " pineapple"; // initialization
+        String pizza_2; // declaration
+        pizza_2 = "bbq";  // assignment
+
+    }
+}
