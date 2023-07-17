a9349efdb3510593b228d35d922f40a789d9c3a9
danielOuattara <daniel.ouattara@gmx.com>
2023-07-17 21:11:22 +0200

    21: type casting

---
+++ src/Java_Basics_01/Java_21_Type_Casting.java
@@ -0,0 +1,28 @@
+package Java_Basics_01;
+
+public class Java_21_Type_Casting {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+
+        int b = (int) 0.5;
+        System.out.println(b);
+
+        double c = 12.0;
+        System.out.println(c);
+
+        int d = (int) c;
+        System.out.println(d);
+
+
+        /*
+        byte
+        short
+        char
+        int
+        long
+        float
+        double
+         */
+    }
+}
