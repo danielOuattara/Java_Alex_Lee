15406968cbb5f50edb1f274ae1b9ab1ce8f0178f
danielOuattara <daniel.ouattara@gmx.com>
2023-07-24 17:22:58 +0200

    16 Math Library

---
+++ src/java_Basics_02/_16_Math_Library.java
@@ -0,0 +1,18 @@
+package java_Basics_02;
+
+public class _16_Math_Library {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.cbrt(64));
+        System.out.println(Math.abs(-23));
+        System.out.println(Math.random());
+        System.out.println((int)(Math.random() * 101) );
+        System.out.println((int)(Math.random() * 100) + 1 );
+        System.out.println(Math.random()* 100 + 1);
+        System.out.println(Math.round(Math.random()* 100) + 1);
+    }
+}
