3301eddfc00c56f1e66b61f56f0d952b5fa177e8
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 23:03:07 +0200

    26 making array of objects

---
+++ src/java_Basics_02/_26_make_an_array_of_object.java
@@ -0,0 +1,24 @@
+package java_Basics_02;
+
+
+class Monkey {
+    String type = "Spider Monkey";
+
+    public  void monkeySound() {
+        System.out.println("Ouhh Aahh Ouhh Aahh !!!");
+    }
+}
+
+public class _26_make_an_array_of_object {
+    public static void main(String[] args) {
+        Monkey monkey_1 = new Monkey();
+        Monkey monkey_2 = new Monkey();
+        Monkey[] monkeysArray = {monkey_1, monkey_2};
+
+        for (Monkey singleMonkey: monkeysArray) {
+            singleMonkey.monkeySound();
+        }
+    }
+
+
+}
