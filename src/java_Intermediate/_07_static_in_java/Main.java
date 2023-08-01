2191077338e3dc795c42426e79388ba645493a31
danielOuattara <daniel.ouattara@gmx.com>
2023-08-01 22:19:33 +0200

    java intermediate: 07 static keyword

---
+++ src/java_Intermediate/_07_static_in_java/Main.java
@@ -0,0 +1,11 @@
+package java_Intermediate._07_static_in_java;
+
+public class Main {
+    static int a =  0 ;
+    static  int months = 12;
+    public static void main(String[] args) {
+        int b = 1;
+        System.out.println(a + b);
+        System.out.println("months = " + months);
+    }
+}
